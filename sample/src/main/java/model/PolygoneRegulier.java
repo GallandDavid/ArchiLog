be8891227a93df51b23f3ff1964e33ea7109719b
David Galland <galland.david33@gmail.com>
2023-02-28 17:30:29 +0100

    no comment

--- sample/src/main/java/model/PolygoneRegulier.java
+++ sample/src/main/java/model/PolygoneRegulier.java
@@ -1,5 +1,9 @@
 package model;
 
 public class PolygoneRegulier extends Forme{
+
+    public PolygoneRegulier(Position position, double rotation, Position centre_rotation, Couleur couleur) {
+        super(position, rotation, centre_rotation, couleur);
+    }
     
 }
--- sample/src/main/java/model/Rectangle.java
+++ sample/src/main/java/model/Rectangle.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Rectangle extends Forme{
+
+    private double largeur;
+    private double hauteur;
+    private double arrondi_bords;
+
+    public Rectangle(Position position, double rotation, Position centre_rotation, Couleur couleur, double largeur, double hauteur, double arrondi_bords) {
+        super(position, rotation, centre_rotation, couleur);
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.arrondi_bords = arrondi_bords;
+    }
+
+    public double getLargeur(){
+        return largeur;
+    }
+
+    public double getHauteur(){
+        return hauteur;
+    }
+
+    public double getArrondiBords(){
+        return arrondi_bords;
+    }
     
 }
