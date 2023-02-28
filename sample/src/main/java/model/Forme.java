99c28cc57ebb55a2233bd4970c617bf064dc8183
David Galland <galland.david33@gmail.com>
2023-02-28 17:26:05 +0100

    implémentre constructeur and getter for Form

--- sample/src/main/java/model/Forme.java
+++ sample/src/main/java/model/Forme.java
@@ -6,4 +6,27 @@ public abstract class Forme {
     private Position centre_rotation;
     private Couleur couleur;
     //private Position translation
+
+    public Forme(Position position, double rotation, Position centre_rotation, Couleur couleur){
+        this.position = position;
+        this.rotation = rotation;
+        this.centre_rotation = centre_rotation;
+        this.couleur = couleur;
+    }
+
+    public Position getPosition(){
+        return position;
+    }
+
+    public double getRotation(){
+        return rotation;
+    }
+
+    public Position getCentreRotation(){
+        return centre_rotation;
+    }
+
+    public Couleur getCouleur(){
+        return couleur;
+    }
 }
