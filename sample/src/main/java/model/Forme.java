82d5058bfab8d0846e87accc1c5e4a4d19c9280d
David Galland <galland.david33@gmail.com>
2023-02-28 17:22:00 +0100

    add position and add attributs for Forme

--- sample/src/main/java/model/Forme.java
+++ sample/src/main/java/model/Forme.java
@@ -1,5 +1,9 @@
 package model;
 
 public abstract class Forme {
-    
+    private Position position;
+    private double rotation;
+    private Position centre_rotation;
+    private Couleur couleur;
+    //private Position translation
 }
