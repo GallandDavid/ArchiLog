f05349759002f7eab5ee8737048cb25599827139
David Galland <galland.david33@gmail.com>
2023-02-28 17:34:00 +0100

    poly

--- sample/src/main/java/model/PolygoneRegulier.java
+++ sample/src/main/java/model/PolygoneRegulier.java
@@ -1,9 +1,22 @@
 package model;
 
 public class PolygoneRegulier extends Forme{
+    private int nb_cote;
+    private double longueur_cote;
 
-    public PolygoneRegulier(Position position, double rotation, Position centre_rotation, Couleur couleur) {
+
+    public PolygoneRegulier(Position position, double rotation, Position centre_rotation, Couleur couleur, int nb_cote, double longueur_cote) {
         super(position, rotation, centre_rotation, couleur);
+        this.longueur_cote = longueur_cote;
+        this.nb_cote = nb_cote;
+    }
+
+    public int getNbCote(){
+        return nb_cote;
+    }
+
+    public double getLongueurCote(){
+        return longueur_cote;
     }
     
 }
