d133a6d022b74bcba4835d485078583332e07c15
Galland David <david.galland@etu.u-bordeaux.fr>
2023-03-30 01:19:20 +0200

    not end

--- src/main/java/xshape/model/shape/RectangleFx.java
+++ src/main/java/xshape/model/shape/RectangleFx.java
@@ -3,7 +3,6 @@ package xshape.model.shape;
 import java.awt.geom.Point2D;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
-import xshape.model.observer.Iobserver;
 
 public class RectangleFx extends Rectangle{
 
@@ -11,29 +10,29 @@ public class RectangleFx extends Rectangle{
     Group _grp;
     
 	public RectangleFx(RectangleFx shape){
-		super((Point2D) shape.position().clone(), (Point2D) shape.size().clone(), (Point2D) shape.visiblePosition().clone(), (Point2D) shape.visibleSize().clone(), shape.isMovable(), shape.getPrevMousePosX(), shape.getPrevMousePosY(), shape.getId(), shape.isPlaced(), shape.deepth(), shape._app);
+		super((Point2D) shape.position().clone(), (Point2D) shape.size().clone(), (Point2D) shape.visiblePosition().clone(), (Point2D) shape.visibleSize().clone(), shape.isMovable(), shape.getPrevMousePosX(), shape.getPrevMousePosY(), shape.getId(), shape.isPlaced(), shape.deepth());
 		_adapted = new javafx.scene.shape.Rectangle();
 		_grp = shape._grp;
 		_grp.getChildren().add(_adapted);
 	}
 
-	public RectangleFx( Group grp, Iobserver obs) {
-		this(_pos_x, _pos_y, _size_y, _size_x, false, grp, obs);
+	public RectangleFx( Group grp) {
+		this(_pos_x, _pos_y, _size_y, _size_x, false, grp);
 	}
-    public RectangleFx(boolean selected, Group grp, Iobserver obs) {
-		this(_pos_x, _pos_y, _size_y, _size_x, selected, grp, obs);
+    public RectangleFx(boolean selected, Group grp) {
+		this(_pos_x, _pos_y, _size_y, _size_x, selected, grp);
 	}
-    public RectangleFx(double posX, double posY, Group grp, Iobserver obs) {
-		this(posX, posY, _size_y, _size_x, false, grp, obs);
+    public RectangleFx(double posX, double posY, Group grp) {
+		this(posX, posY, _size_y, _size_x, false, grp);
 	}
-    public RectangleFx(double posX, double posY, boolean selected, Group grp, Iobserver obs) {
-		this(posX, posY, _size_y, _size_x, selected, grp, obs);
+    public RectangleFx(double posX, double posY, boolean selected, Group grp) {
+		this(posX, posY, _size_y, _size_x, selected, grp);
 	}
-    public RectangleFx(double posX, double posY, double height, double width, Group grp, Iobserver obs) {
-		this(posX, posY, height, width, false, grp, obs);
+    public RectangleFx(double posX, double posY, double height, double width, Group grp) {
+		this(posX, posY, height, width, false, grp);
 	}
-    public RectangleFx(double posX, double posY, double height, double width, boolean selected, Group grp, Iobserver obs) {
-		super(new Point2D.Double(posX, posY),new Point2D.Double(width, height), selected, obs);
+    public RectangleFx(double posX, double posY, double height, double width, boolean selected, Group grp) {
+		super(new Point2D.Double(posX, posY),new Point2D.Double(width, height), selected);
 		_adapted = new javafx.scene.shape.Rectangle();
 		_grp = grp;
 		_grp.getChildren().add(_adapted);
@@ -41,14 +40,9 @@ public class RectangleFx extends Rectangle{
 
 	@Override
 	public void draw() {
-		if(!_grp.
-		getChildren().
-		contains(
-			_adapted))
-			_grp.getChildren().add(_adapted);
+		if(!_grp.getChildren().contains(_adapted)) _grp.getChildren().add(_adapted);
 		Point2D p = visiblePosition();
 		Point2D	s = visibleSize();
-		
 		_adapted.setX(p.getX()- s.getX()/2);
 		_adapted.setY(p.getY()- s.getY()/2);
 		_adapted.setWidth(s.getX());
@@ -69,11 +63,6 @@ public class RectangleFx extends Rectangle{
     public boolean equals(Object obj){
 		return super.equals(obj);
 	}
-
-	@Override
-	public boolean isInside(Point2D pos){
-        return pos.getX() > position().getX() - size().getX() / 2 && pos.getX() < position().getX() + size().getX() / 2 && pos.getY() > position().getY() - size().getY() / 2 && pos.getY() < position().getY() + size().getY() / 2;
-    }
 
 	@Override
 	public Object adapted() {
